08ee1292adf0dfe4fb42a4ec82c096ecfb655ea0
abaranovskydako-tech <a.baranovsky.dako@gmail.com>
2026-01-28 12:42:31 +0300

    Add Quiz and AI Widget components, create route pages, add types and README

---
+++ components/quiz/QuizWidget.tsx
@@ -0,0 +1,5 @@
+type QuizWidgetProps = { onSubmit?: (data: unknown) => void };
+
+export default function QuizWidget({ onSubmit }: QuizWidgetProps) {
+  return <section><div>Quiz widget placeholder</div></section>;
+}
