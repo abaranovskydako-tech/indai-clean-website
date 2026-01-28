e9ba6d404448ff0d2935c2777da8ee055ba3b5ee
abaranovskydako-tech <a.baranovsky.dako@gmail.com>
2026-01-28 13:26:42 +0300

    Update app/cases/page.tsx - refactor to stub with TODO
    Remove existing JSX structure and return null for now.

--- app/cases/page.tsx
+++ app/cases/page.tsx
@@ -1,8 +1,6 @@
+import React from "react";
+
 export default function CasesPage() {
-  return (
-    <main>
-      <h1>Cases</h1>
-      <p>Case studies and projects</p>
-    </main>
-  );
+  // TODO: compose cases blocks from components according to /docs
+  return null;
 }
